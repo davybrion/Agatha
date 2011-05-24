bf76090666ee9ecd4f5f6a43f825b1577751ee13
Bart Deleye <bart.deleye@gmail.com>
2011-05-24 23:54:37 +0200

    Created rake build script

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,13 +1,36 @@
 require 'albacore'
 
-msbuild :build do |msb|
+version = "1.2.9"
+common_dir = "Common"
+release_dir = "release"
+build_configuration = "Release"
+
+assemblyinfo :assemblyinfo do |asm|
+	Dir.mkdir(common_dir) unless File.directory?(common_dir)
+	asm.version = version
+	asm.file_version = version
+	asm.product_name = "Agatha"
+	asm.copyright = "Copyright (C) Davy Brion"
+	asm.output_file = "#{common_dir}/CommonAssemblyInfo.cs"
+end
+
+msbuild :build => :assemblyinfo do |msb|
 	msb.solution = "Agatha.sln"
 	msb.targets :clean, :build
-	msb.properties :configuration => :release
+	msb.properties :configuration => build_configuration
 end
 
-task :test do
-	puts "running tests"
+xunit :test => :build do |xunit|
+	xunit.command = "libs/xunit.net/xunit.console.clr4.exe"
+	xunit.assembly = "Tests/bin/release/Tests.dll"
 end
 
-task :default => [:build, :test]
+zip :package do |zip|
+	Dir.mkdir(release_dir) unless File.directory?(release_dir)
+	zip.directories_to_zip "Agatha.ServiceLayer/Bin/#{build_configuration}"
+	zip.additional_files = "changelog.txt","contributors.txt","license.txt"
+	zip.output_file = release_dir + "/Agatha_#{version}.zip"
+	zip.output_path = File.dirname(__FILE__)
+end
+
+task :default => [:test,:package]
