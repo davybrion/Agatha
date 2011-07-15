79659371511351fe8ad62bc4d35d7eca6f28ac1e
Davy Brion <ralinx@davybrion.com>
2011-07-15 22:39:47 +0200

    replaced unity dependency with nuget package

--- examples/Sample.SilverlightClient/ComponentRegistration.cs
+++ examples/Sample.SilverlightClient/ComponentRegistration.cs
@@ -7,9 +7,9 @@ namespace Sample.SilverlightClient
 	{
 		public static void Register()
 		{
-			//new ClientConfiguration(typeof(HelloWorldRequest).Assembly, typeof(Agatha.Unity.Container)).Initialize();
+			new ClientConfiguration(typeof(HelloWorldRequest).Assembly, typeof(Agatha.Unity.Container)).Initialize();
 			//new ClientConfiguration(typeof(HelloWorldRequest).Assembly, typeof(Agatha.Castle.Container)).Initialize();
-			new ClientConfiguration(typeof(HelloWorldRequest).Assembly, typeof(Agatha.Ninject.Container)).Initialize();
+			//new ClientConfiguration(typeof(HelloWorldRequest).Assembly, typeof(Agatha.Ninject.Container)).Initialize();
 		}
 	}
 }
