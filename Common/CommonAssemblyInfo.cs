0d346d7fb0fd7dd904d989a8e8265dc06968329c
whut <rafalklys@wp.pl>
2011-07-27 20:05:37 +0200

    Commiting CommonAssemblyInfo.cs, this way you can compile project just from Visual Studio

---
+++ Common/CommonAssemblyInfo.cs
@@ -0,0 +1,7 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+[assembly: AssemblyProduct("Agatha")]
+[assembly: AssemblyCopyright("Copyright (C) Davy Brion")]
+[assembly: AssemblyVersion("1.3.0")]
+[assembly: AssemblyFileVersion("1.3.0")]
+
